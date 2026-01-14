dc0d64caa2d05fc31698c72ef304647b3312b911
Anina Angelova <anina_1999@abv.bg>
2026-01-14 22:02:55 +0200

    Upload clean SPA exercise
    Built with modular architecture, routing with page.js and template engine - lit.html

---
+++ Exercise Modular Application/Exam Preparation 8 SEARCH/EXAM Prep/src/app.js
@@ -0,0 +1,30 @@
+import { decorateContext } from './utility/decorateContext.js';
+import { page } from './utility/library.js';
+import { logout } from './utility/logout.js';
+import { updateNav } from './utility/navigationControl.js';
+import { showCreateView } from './views/createView.js';
+import { showDashboardView } from './views/dashboardView.js';
+import { deleteHandler } from './views/deleteView.js';
+import { showDetailsView } from './views/detailsView.js';
+import { showEditView } from './views/editView.js';
+import { showHomePageView } from './views/homePageView.js';
+import { showLoginView } from './views/loginView.js';
+import { showRegisterView } from './views/registerView.js';
+import { showSearchView } from './views/searchView.js';
+
+page(decorateContext);
+
+page('/', showHomePageView);
+page('/dashboard', showDashboardView);
+page('/login', showLoginView);
+page('/register', showRegisterView);
+page('/create', showCreateView);
+page('/details/:id', showDetailsView);
+page('/edit/:id', showEditView);
+page('/delete/:id', deleteHandler);
+page('/search', showSearchView);
+page('/logout', logout);
+
+page.start();
+
+updateNav();
---
+++ Exercise Modular Application/Exam Preparation 8 SEARCH/EXAM Prep/src/utility/navigationControl.js
@@ -0,0 +1,28 @@
+import { html, render } from './library.js';
+import { userUtils } from './userUtils.js';
+
+const root = document.querySelector('nav');
+const temp = (hasUser) => html`
+        <div>
+            <a href="/dashboard">Our Cars</a>
+            <a href="/search">Search</a>
+        </div>
+    ${hasUser ?
+        html`
+          <div class="user">
+            <a href="/create">Add Your Car</a>
+            <a href="/logout">Logout</a>
+          </div>`
+        : html`
+          <div class="guest">
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+          </div>`
+    }
+`
+
+export function updateNav() {
+    const user = userUtils.getUserId();
+
+    render(temp(!!user), root);
+}
